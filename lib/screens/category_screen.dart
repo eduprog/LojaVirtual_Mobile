b95b99dca73c0b0361494e2abdb398202cb87662
Jonathas Tassi e Silva <jonathastassi@hotmail.com>
2020-04-26 21:38:14 -0300

    Refatorando a tela de categoria e criando um service para separar a lógica da API e da tela

--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:lojavirtual_mobile/models/category.model.dart';
 import 'package:lojavirtual_mobile/models/product.model.dart';
 import 'package:http/http.dart' as http;
+import 'package:lojavirtual_mobile/services/category.service.dart';
 import 'dart:convert' as convert;
 
 import 'package:lojavirtual_mobile/services/utils/response.api.model.dart';
@@ -11,20 +12,6 @@ class CategoryScreen extends StatelessWidget {
   final CategoryModel _categoryModel;
 
   CategoryScreen(this._categoryModel);
-
-  Future<List<ProductModel>> getProducts(String idCategory) async {
-    http.Response response = await http.get(
-        "https://192.168.0.104:5001/api/category/" + idCategory + "/products");
-
-    ResponseApi jsonResponse =
-        ResponseApi.fromJson(convert.jsonDecode(response.body));
-
-    if (jsonResponse.success == true) {
-      return (jsonResponse.data)
-          .map((banners) => ProductModel.fromJson(banners))
-          .toList();
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -47,7 +34,7 @@ class CategoryScreen extends StatelessWidget {
             ),
           ),
           body: FutureBuilder<List<ProductModel>>(
-            future: getProducts(_categoryModel.id),
+            future: CategoryService.getProducts(_categoryModel.id),
             builder: (context, snapshot) {
               if (snapshot.connectionState == ConnectionState.none ||
                   snapshot.connectionState == ConnectionState.waiting) {
---
+++ lib/services/category.service.dart
@@ -0,0 +1,25 @@
+import 'package:lojavirtual_mobile/models/category.model.dart';
+import 'package:lojavirtual_mobile/models/product.model.dart';
+import 'package:lojavirtual_mobile/services/utils/Api.dart';
+
+class CategoryService {
+  static Future<List<CategoryModel>> getCategories() async {
+    var response = await Api.get("api/category");
+
+    if (response["success"] == true) {
+      return (response["data"] as List)
+          .map((banners) => CategoryModel.fromJson(banners))
+          .toList();
+    }
+  }
+
+  static Future<List<ProductModel>> getProducts(String idCategory) async {
+    var response = await Api.get("api/category/" + idCategory + "/products");
+
+    if (response["success"] == true) {
+      return (response["data"] as List)
+          .map((banners) => ProductModel.fromJson(banners))
+          .toList();
+    }
+  }
+}
